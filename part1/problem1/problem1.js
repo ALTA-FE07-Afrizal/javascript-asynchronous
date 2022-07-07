d581411d805a820f246e84433a45a635892ec45d
unknown <4friizal@gmail.com>
2022-07-07 08:54:03 +0700

    problem 1 part 1

--- part1/problem1/problem1.js
+++ part1/problem1/problem1.js
@@ -1,9 +1,8 @@
 function jajanBoba(uang, callback) {
   const hargaBoba = 5000;
-  let sisaUang = "";
+  let sisaUang = uang - hargaBoba;
   setTimeout(() => {
     if (uang >= hargaBoba) {
-      sisaUang = uang - hargaBoba;
       console.log(
         `kamu jajan boba dengan harga Rp.${hargaBoba}\nsisa uang kamu Rp.${sisaUang}`
       );
@@ -12,23 +11,26 @@ function jajanBoba(uang, callback) {
         `maaf uang kamu belum cukup untuk membeli Boba\nsisa uang kamu sebesar Rp.${uang}`
       );
     }
-  }, 1000);
+  }, 5000);
   callback(sisaUang);
 }
 
 function jajanSeblak(uang) {
   const hargaSeblak = 8000;
-  let sisaUang = "";
+  let sisaUang = uang - hargaSeblak;
   setTimeout(() => {
     if (uang >= hargaSeblak) {
-      sisaUang = uang - hargaSeblak;
       console.log(
         `kamu jajan boba dengan harga Rp.${hargaSeblak}\nsisa uang kamu Rp.${sisaUang}`
       );
+    } else {
+      console.log(
+        `maaf uang kamu belum cukup untuk membeli Seblak\nsisa uang kamu sebesar Rp.${uang}`
+      );
     }
-  }, 3000);
+  }, 9000);
 }
 
-// jajanSeblak(10000);
-
+// jajanSeblak(7000);
 jajanBoba(20000, jajanSeblak);
+
